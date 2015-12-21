0ad7a2f256f6c1e8734cb79cd60e8c456d056bfa
LMCMLJ <mjcalvey2@gmail.com>
2015-12-20 18:21:56 -0700

    Skeleton setup
    —

---
+++ Setup.py
@@ -0,0 +1,16 @@
+setup(
+    name='Vendor',
+    version='0.0.1',
+    description='Vending machine software',
+    long_description=long_description,
+    url='https://github.com/LMCMLJ/samnple',
+    author='Michael Calvey',
+    license='Apache_2.0',
+    classifiers=[
+                   'Development Status :: 1 - pre-dev',
+                   'Intended Audience :: commercial',
+                   'Topic :: sample :: sample',
+                   'License :: Apache 2.0',
+                   'Programming Language :: Python :: 3.x',
+    ]
+)
