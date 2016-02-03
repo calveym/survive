b98adc0ba0e942537aee894e9b82b195ec5137ee
LMCMLJ <mjcalvey2@gmail.com>
2016-02-03 00:20:06 +0000

    Locations
    Added some locations

--- main/data.py
+++ main/data.py
@@ -1,5 +1,27 @@
 buildings = {'shelter': 0,
              'shed': 0,
              'corral': 0,
-             '': 0
+             'workshop': 0,
+             'outpost': 0
              }
+
+locations = {'Volga banks': {'urban': False, 'difficulty': 1, 'wood': 50, 'food': 30, 'freshwater': 50,
+                             'dirtywater': 0, 'polluted': False},
+             'Pine forest': {'urban': False, 'difficulty': 1, 'wood': 75, 'food': 25, 'freshwater': 5,
+                             'dirtywater': 0, 'polluted': False},
+             'Belka E': {'urban': False, 'difficulty': 2, 'wood': 50, 'food': 20, 'freshwater': 5,
+                         'dirtywater': 2, 'polluted': False},
+             'Belka W': {'urban': False, 'difficulty': 3, 'wood': 40, 'food': 15, 'freshwater': 5,
+                         'dirtywater': 5, 'polluted': False},
+             'Blue hills E': {'urban': False, 'difficulty': 3, 'wood': 20, 'food': 10, 'freshwater': 0,
+                              'dirtywater': 10, 'polluted': False},
+             'Blue hills W': {'urban': False, 'difficulty': 4, 'wood': 20, 'food': 0, 'freshwater': 0,
+                              'dirtywater': 20, 'polluted': False},
+             'Lenin bridge': {'urban': True, 'difficulty': 5, 'wood': 0, 'food': 5, 'freshwater': 10,
+                              'dirtywater': 30, 'polluted': True},
+             'Park Svobodi': {'urban': True, 'difficulty': 4, 'wood': 0, 'food': 15, 'freshwater': 15,
+                              'dirtywater': 60, 'polluted': True},
+             'Zhukov industrial estate': {'urban': True, 'difficulty': 5, 'wood': 50, 'food': 0, 'freshwater': 0,
+                                          'dirtywater': 90, 'polluted': True}
+
+}
